ad38f513b63210056ec841db950181e7fdd2044d
Amelia Glenn <amelia.glenn494@gmail.com>
2020-02-08 09:18:18 -0500

    rename and slight update
    renamed makerspaceLambda->lambda_function so upload to AWS is smoother; test upload of dictionaryCreator; addition of current version of the inventory spreadsheet

--- dictionaryCreator.py
+++ dictionaryCreator.py
@@ -1,4 +1,9 @@
 import pandas as pd
-inventory=pd.read_excel(r'C:\Users\ameli\Downloads\Alexa Skills Inventory.xlsx')
+inventory=pd.read_excel(r'C:\Users\ameli\Documents\GitHub\notJarvis\Alexa Skills Inventory.xlsx')
 location=pd.DataFrame(inventory, columns=['Item Name','Location in G34'])
-synonyms=pd.DataFrame(inventory, columns=['Alternative Names','Item Name'])
+synonyms=pd.DataFrame(inventory, columns=['Alternative Names'])
+#items=pd.DataFrame(inventory, columns=['Item Name'])
+location.to_dict()
+print(type(location))
+print(location)
+#print(location['0'])
