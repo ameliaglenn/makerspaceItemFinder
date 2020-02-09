b32884d2c3bad905ad6fd7f3168c7571adfbd16b
Amelia Glenn <amelia.glenn494@gmail.com>
2020-02-09 17:40:50 -0500

    dictionary created

--- dictionaryCreator.py
+++ dictionaryCreator.py
@@ -2,8 +2,18 @@ import pandas as pd
 inventory=pd.read_excel(r'C:\Users\ameli\Documents\GitHub\notJarvis\Alexa Skills Inventory.xlsx')
 location=pd.DataFrame(inventory, columns=['Item Name','Location in G34'])
 synonyms=pd.DataFrame(inventory, columns=['Alternative Names'])
-#items=pd.DataFrame(inventory, columns=['Item Name'])
-location.to_dict()
-print(type(location))
-print(location)
-#print(location['0'])
+totalRows=len(inventory.axes[0])
+l=location.to_dict(orient='list')
+s=synonyms.values.tolist()
+print(s[0])
+loc={}
+syn={}
+for i in range(totalRows):
+    specificSyn=''.join(s[i])
+    synonymList=specificSyn.split(', ')
+    totSyn=len(synonymList)
+    print(synonymList)
+    print(totSyn)
+    loc[l['Item Name'][i]]=l['Location in G34'][i]
+    for k in range(totSyn):
+        syn[synonymList[k]]=l['Item Name'][i]
